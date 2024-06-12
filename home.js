f4da61680f714cb2efe907466a2c34bfbd239359
Ankit Kumar <122714569+ankitkr0408@users.noreply.github.com>
2024-06-13 03:28:57 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,13 @@
+{/* <img src="./assests/image/logo.png" alt="this is the 1st centered image"> </div>
+    <div class="bottom-left">
+        <p>Successfully Mentored <br>
+        Research Scholars & Authors <br>
+        across the Globe <br></p> 
+        <button type="button">Click Me!</button>
+    </div>
+    
+        <h3>
+            Why phD Assistance <br>
+            A Trusted & Academically Sound Mentors representing various Countries
+        </h3>
+        <h1>sdf</h1> */}

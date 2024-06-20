3ed40c1ca960b6c582a75d012b0ccc016e0b7a97
Ankit Kumar <122714569+ankitkr0408@users.noreply.github.com>
2024-06-21 04:16:23 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,13 +1,43 @@
-{/* <img src="./assests/image/logo.png" alt="this is the 1st centered image"> </div>
-    <div class="bottom-left">
-        <p>Successfully Mentored <br>
-        Research Scholars & Authors <br>
-        across the Globe <br></p> 
-        <button type="button">Click Me!</button>
-    </div>
-    
-        <h3>
-            Why phD Assistance <br>
-            A Trusted & Academically Sound Mentors representing various Countries
-        </h3>
-        <h1>sdf</h1> */}
+document.addEventListener('DOMContentLoaded', function() {
+    const swipeArea = document.querySelector('.main-carousel');
+    const carousel = swipeArea.querySelector('.carousel');
+    let startX, startY;
+
+    swipeArea.addEventListener('mousedown', startDrag);
+    swipeArea.addEventListener('mouseup', endDrag);
+    swipeArea.addEventListener('touchstart', startDrag);
+    swipeArea.addEventListener('touchend', endDrag);
+
+    function startDrag(e) {
+        if (e.type === 'touchstart') {
+            startX = e.touches[0].clientX;
+            startY = e.touches[0].clientY;
+        } else {
+            startX = e.clientX;
+            startY = e.clientY;
+        }
+    }
+
+    function endDrag(e) {
+        let endX, endY;
+
+        if (e.type === 'touchend') {
+            endX = e.changedTouches[0].clientX;
+            endY = e.changedTouches[0].clientY;
+        } else {
+            endX = e.clientX;
+            endY = e.clientY;
+        }
+
+        let diffX = startX - endX;
+        let diffY = startY - endY;
+
+        if (Math.abs(diffX) > Math.abs(diffY)) {
+            if (diffX > 0) {
+                carousel.scrollLeft += 300;
+            } else {
+                carousel.scrollLeft -= 300;
+            }
+        }
+    }
+});
